fc6f1878e99fd60943e404f1e7b2337f86ff9827
aoki_shou <aoki.shou@it-sp.co.jp>
2024-08-13 21:57:07 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,8 +2,22 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 1から100までのループ
+		for (int i = 1; i <= 100; i++) {
+			// 3と5の両方で割り切れる数値の場合、「FizzBuzz」と表示
+			if (i % 3 == 0 && i % 5 == 0)
+				System.out.println("FizzBuzz");
 
+			// 3で割り切れる数値の場合、「Fizz」と表示する
+			else if (i % 3 == 0)
+				System.out.println("Fizz");
+
+			// 5で割り切れる数値の場合、「Buzz」と表示する
+			else if (i % 5 == 0)
+				System.out.println("Buzz");
+
+			// それ以外の数値は、その数値をそのまま表示
+			System.out.println(i);
+		}
 	}
-
 }
